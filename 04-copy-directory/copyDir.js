26908fcb6df41e75cd4d0fc41168e2a3290b4c23
maxvy50rs <vysokov.maksim95@yandex.ru>
2023-05-08 21:43:15 +0600

    refactor: make copyDir reusable

---
+++ 04-copy-directory/copyDir.js
@@ -0,0 +1,17 @@
+const { mkdir, readdir, copyFile, rm } = require('fs/promises');
+const path = require('path');
+
+module.exports.copyDir = async function copyDir(dir, dirCopy) {
+  await rm(dirCopy, { recursive: true, force: true });
+  await mkdir(dirCopy, { recursive: true }, (err) => {
+    if (err) throw err;
+  });
+
+  const content = await readdir(dir, { withFileTypes: true });
+  content.forEach(async item => {
+    const src = path.join(dir, item.name);
+    const dest = path.join(dirCopy, item.name);
+    if (item.isDirectory()) copyDir(item.name);
+    await copyFile(src, dest);
+  });
+};
